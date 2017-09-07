0d03d28c2e390c47f05a9081c4a4448e7a450c71
coleworsley <cole.s.worsley@gmail.com>
2017-09-07 14:20:43 -0600

    add extra homework examples

--- public/index.js
+++ public/index.js
@@ -1,2 +1,39 @@
-console.log('hooked up!');
 const socket = io();
+let user = ''
+
+$('form').submit(() => {
+  socket.emit('chat message', $('#m').val());
+  $('#m').val('');
+  return false;
+});
+
+socket.on('chat message', (messageObj) => {
+  user = messageObj.user;
+  $('#messages').append($('<li>').text(`${messageObj.user}: ${messageObj.message}`));
+});
+
+socket.on('users', (users) => {
+  $('.users').text(`
+    Connected Users: ${users}
+  `)
+})
+
+$('#m').on('keyup', (e) => {
+    console.log(e.target.value.length);
+    if (e.target.value.length > 0) {
+      socket.emit('typing', {
+        message: 'is typing...',
+        user,
+      });
+    } else {
+      socket.emit('typing', {
+        message: 'Type Something!',
+        user: '',
+      })
+    }
+
+})
+
+socket.on('typing', (messageObj) => {
+  $('#m').prop('placeholder',`${messageObj.user} ${messageObj.message}`);
+})
--- server.js
+++ server.js
@@ -3,12 +3,41 @@ const app = express();
 const http = require('http').Server(app);
 const port = process.env.PORT || 3000;
 const io = require('socket.io')(http);
+const path = require('path');
+const UsernameGenerator = require('username-generator');
+
+const users = [];
 
 app.use(express.static('public'))
 
-io.on('connection', (socket) => {
-  console.log('user connected');
+io.on('connection', function(socket){
+
+  const user = UsernameGenerator.generateUsername();
+  const message = 'has connected. Say hello!'
+  users.push(user);
+  io.emit('chat message', { user, message })
+  io.emit('users', users);
+
+  socket.on('disconnect', function(){
+    if (users.indexOf(user) > -1) {
+      users.splice(users.indexOf(user), 1);
+    }
+    io.emit('chat message', { user, message: 'has disconnected' });
+    io.emit('users', users);
+  });
+
+  socket.on('chat message', (message) => {
+    io.emit('chat message', { user, message });
+  });
+
+  socket.on('typing', (messageObj) => {
+    io.emit('typing', messageObj);
+  })
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+})
 
 http.listen(3000, () => {
   console.log(`listening on localhost:${port}`);
